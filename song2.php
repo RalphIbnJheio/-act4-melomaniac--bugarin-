1332aea4703ea8dcde645185064193609ba3a83a
BugarinRalph <dyj12.oryxa@gmail.com>
2022-04-16 16:40:55 +0800

    second

---
+++ song2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="http://localhost/melomaniac-activity/index.php"> Listen Up </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="http://localhost/melomaniac-activity/song2.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">The Soul Of Life</h1>
+                <p class="lead">Music is a vital part of different moments of human life. It spreads happiness and joy in a person’s life.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center"> 
+
+                        <?php
+                        echo "<h2 class=>In Summer</h2>";
+                        echo "<p class=>Josh Gad</p>";
+
+
+                        echo "<p> Bees'll buzz, kids'll blow dandelion fuzz </p>";
+                        echo "<p>And I'll be doing whatever snow does in summer.</p>";
+                        echo "<p>A drink in my hand, my snow up against the burning sand</p>";
+                        echo "<p>Probably getting gorgeously tanned in summer.</p>";
+                        echo "<br/>";
+
+                        echo "<p>I'll finally see a summer breeze, blow away a winter storm</p>";
+                        echo "<p>And find out what happens to solid water when it gets warm!</p>";
+                        echo "<p>And I can't wait to see, what my buddies all think of me.</p>";
+                        echo "<p>Just imagine how much cooler I'll be in summer.</p>";
+                        echo "<br/>";
+
+                        echo "<p>Dah dah, da doo, uh bah bah bah bah bah wooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> The hot and the cold are both so intense,</p>";
+                        echo "<p> Put 'em together it just makes sense! </p>";
+                        echo "<br/>";
+
+                        echo "<p>Rrr Raht da daht dah dah dah dah dah doo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Winter's a good time to stay in and cuddle, </p>";
+                        echo "<p> But put me in summer and I'll be a - happy snowman! </p>";
+                        echo "<br/>";
+
+                        echo "<p> When life gets rough, I like to hold on to my dream,</p>";
+                        echo "<p> Of relaxing in the summer sun, just lettin' off steam.</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh the sky will be blue, and you guys will be there too</p>";
+                        echo "<p> When I finally do what frozen things do in summer.</p>";
+                        echo "<br/>";
+                        ?>
+      
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ralph Ibn Jheio Bugarin</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
