039d15a06d61a0d62aeaafb2b0bdc7e8db109028
BugarinRalph <dyj12.oryxa@gmail.com>
2022-04-16 17:28:50 +0800

    third

---
+++ song3.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="http://localhost/melomaniac-activity/index.php"> Listen Up </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="http://localhost/melomaniac-activity/song2.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="http://localhost/melomaniac-activity/song3.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">The Soul Of Life</h1>
+                <p class="lead">Music is a vital part of different moments of human life. It spreads happiness and joy in a person’s life.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center"> 
+
+                        <?php
+                        echo "<h2 class=>Mistletoe </h2>";
+                        echo "<p class=>Justin Bieber </p>";
+
+
+                        echo "<p> It’s the most beautiful time of the year </p>";
+                        echo "<p> Lights fill the streets spreading so much cheer </p>";
+                        echo "<p> I should be playing in the winter snow </p>";
+                        echo "<p> But I'mma be under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>I don’t wanna miss out on the holiday</p>";
+                        echo "<p>But I can’t stop staring at your fac</p>";
+                        echo "<p>I should be playing in the winter snow</p>";
+                        echo "<p>But I’mma be under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p> Everyone's gathering around the fire,</p>";
+                        echo "<p> Chestnuts roasting like a hot July </p>";
+                        echo "<p> I should be chilling with my folks, I know </p>";
+                        echo "<p> But I’mma be under the mistletoe </p>";
+                        echo "<br/>";
+
+                        echo "<p>Word on the street Santa's coming tonight,</p>";
+                        echo "<p>Reindeer's flying through the sky so high</p>";
+                        echo "<p>I should be making a list, I know</p>";
+                        echo "<p>But I’mma be under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p> Aye, love, the wise men followed the star </p>";
+                        echo "<p> The way I followed my heart </p>";
+                        echo "<p> And it led me to a miracle </p>";
+                        echo "<br/>";
+
+                        echo "<p> Aye, love, don't you buy me nothing </p>";
+                        echo "<p> 'cause I am feeling one thing, your lips on my lips </p>";
+                        echo "<p> That’s a merry, merry Christmas </p>";
+                        echo "<br/>";
+
+                        echo "<p> It’s the most beautiful time of the year </p>";
+                        echo "<p> Lights fill the streets spreading so much cheer </p>";
+                        echo "<p> I should be playing in the winter snow </p>";
+                        echo "<p> But I'mma be under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>I don’t wanna miss out on the holiday</p>";
+                        echo "<p>But I can’t stop staring at your fac</p>";
+                        echo "<p>I should be playing in the winter snow</p>";
+                        echo "<p>But I’mma be under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, shawty with you</p>";
+                        echo "<p>With you, under the mistletoe, under the mistletoe</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kiss me underneath the mistletoe</p>";
+                        echo "<p>Show me baby that you love me so-oh-oh</p>";
+                        echo "<p> Oh, oh, ohhh </p>";
+                        echo "<br/>";
+
+                        echo "<p> Kiss me underneath the mistletoe</p>";
+                        echo "<p>Show me baby that you love me so-oh-oh</p>";
+                        echo "<p> Oh, oh, ohhh </p>";
+                        echo "<br/>";
+                        ?>
+      
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ralph Ibn Jheio Bugarin</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
