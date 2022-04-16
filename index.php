ebb826bb6d0d1318f2c3c8bacafb3f7e188fb23c
BugarinRalph <dyj12.oryxa@gmail.com>
2022-04-16 16:00:37 +0800

    commited

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Listen Up </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">The Soul Of Life</h1>
+                <p class="lead">Music is a vital part of different moments of human life. It spreads happiness and joy in a person’s life.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center"> 
+
+                        <?php
+                        echo "<h2 class=>Never Enough</h2>";
+                        echo "<p class=>Loren Allred</p>";
+
+
+                        echo "<p>I'm tryin' to hold my breath </p>";
+                        echo "<p>Let it stay this way</p>";
+                        echo "<p>Can't let this moment end</p>";
+                        echo "<p>You set off a dream in me </p>";
+                        echo "<p>Gettin' louder now</p>";
+                        echo "<p>Can you hear it echoing?</p>";
+                        echo "<p>Take my hand</p>";
+                        echo "<p>Will you share this with me?</p>";
+                        echo "<p>'Cause darling without you</p>";
+                        echo "<br/>";
+
+                        echo "<p>All the shine of a thousand spotlights</p>";
+                        echo "<p> All the stars we steal from the night sky</p>";
+                        echo "<p> Will never be enough </p>";
+                        echo "<p> Never be enough </p>";
+                        echo "<p> Towers of gold are still too little </p>";
+                        echo "<p> These hands could hold the world but it'll </p>";
+                        echo "<p> Never be enough </p>";
+                        echo "<p> Never be enough? </p>";
+                        echo "<br/>";
+
+                        echo "<p> For me </p>";
+                        echo "<p> Never, never </p>";
+                        echo "<p> Never, never </p>";
+                        echo "<p> Never, for me</p>";
+                        echo "<p> For me </p>";
+                        echo "<p> Never enough </p>";
+                        echo "<p> Never enough </p>";
+                        echo "<p> Never enough</p>";
+                        echo "<p> For me </p>";
+                        echo "<p> For me </p>";
+                        echo "<p> For me </p>";
+                        echo "<br/>";
+
+                        echo "<p> All the shine of a thousand spotlights</p>";
+                        echo "<p> All the stars we steal from the night sky</p>";
+                        echo "<p> Will never be enough </p>";
+                        echo "<p> Never be enough </p>";
+                        echo "<p> Towers of gold are still too little </p>";
+                        echo "<p> These hands could hold the world but it'll </p>";
+                        echo "<p> Never be enough </p>";
+                        echo "<p> Never be enough? </p>";
+                        echo "<br/>";
+
+                        echo "<p> For me </p>";
+                        echo "<p> Never, never </p>";
+                        echo "<p> Never, never </p>";
+                        echo "<p> Never, for me</p>";
+                        echo "<p> For me </p>";
+                        echo "<p> Never enough </p>";
+                        echo "<p> Never enough </p>";
+                        echo "<p> Never enough</p>";
+                        echo "<p> Never enough</p>";
+                        echo "<p> Never enough</p>";
+                        echo "<p> For me </p>";
+                        echo "<p> For me </p>";
+                        echo "<p> For me </p>";
+                        echo "<br/>";
+
+                        echo "<p> Never enough</p>";
+                        ?>
+      
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ralph Ibn Jheio Bugarin</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
